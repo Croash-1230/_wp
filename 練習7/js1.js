01c815ad6031f3b22a242788791bb684d7deceb9
Croash-1230 <sherry38775883@gmail.com>
2026-06-11 04:20:56 +0800

    Create js1.js

---
+++ 練習7/js1.js
@@ -0,0 +1,12 @@
+// 宣告 post 物件
+const post = {
+  id: 1,
+  title: "Hello World",
+  content: "Markdown content"
+};
+
+// 使用 Dot notation（點符號）印出 title
+console.log(post.title);
+
+// 使用 Bracket notation（中括號）印出 title
+console.log(post["title"]);
