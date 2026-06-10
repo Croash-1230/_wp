d05f514d4e18414704beea699ee7672dd5075101
Croash-1230 <sherry38775883@gmail.com>
2026-06-11 02:33:38 +0800

    Create js9.js

---
+++ 練習4/js9.js
@@ -0,0 +1,13 @@
+let students = [
+    { name: "小明", score: 90 },
+    { name: "小華", score: 80 },
+    { name: "小美", score: 95 }
+];
+
+for (let i = 0; i < students.length; i++) {
+    console.log(
+        students[i].name +
+        "：" +
+        students[i].score
+    );
+}
